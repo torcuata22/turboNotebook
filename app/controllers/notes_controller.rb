ec687c90555b5c9ea06d3ca5a9748edcba5dab2a
torcuata22 <marilynjmarquez@gmail.com>
2023-07-22 15:22:31 -0400

    crud

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -55,6 +55,7 @@ class NotesController < ApplicationController
     @note.destroy
 
     respond_to do |format|
+      format.turbo_stream 
       format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
       format.json { head :no_content }
     end
